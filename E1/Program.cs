44800e713e4def22b72bbe11ada1293ba4032d93
Nataly <tusikf@yandex.ru>
2023-07-03 22:21:36 +0300

    done 1

--- E1/Program.cs
+++ E1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int schet = 0;
+
+int[] array = GetArray(size);
+Console.WriteLine($"Исходный массив: [{String.Join(", ", array)}]");
+
+foreach(int chislo in array)
+{
+    schet += chislo % 2 == 0 ? 1 : 0;
+}
+
+Console.WriteLine($"Количество чётных элементов в массиве = { schet}");
+
+// Функция заполнения массива
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
